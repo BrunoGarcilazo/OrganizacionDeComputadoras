4d05b5f8c74ebfa86b59a2140b0d54186c83b2d2
FelipeMestre <feli13mestre@gmail.com>
2019-09-13 21:33:53 -0300

    laboratorio2 proyecto vacio

--- Laboratorio2.X/Laboratorio2.asm
+++ Laboratorio2.X/Laboratorio2.asm
@@ -1,3 +1,27 @@
+; PIC16F887 Configuration Bit Settings
+; Assembly source line config statements
+#include "p16f887.inc"
+    
+; Bits de configuracion
 
+; CONFIG1
+; __config 0xE0C2
+ __CONFIG _CONFIG1, _FOSC_HS & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFEFF
+ __CONFIG _CONFIG2, _BOR4V_BOR21V & _WRT_OFF
+ 
+;Organizacion de la memoria de datos 
+cblock 0x20	;Comienzo a escribir la memoria de datos en la dirección 0x20 
+endc
 
+;Organizacion de la memoria de programacion
+org 0x0000
 
+main
+    
+__main
+    
+    goto __main
+ 
+END
