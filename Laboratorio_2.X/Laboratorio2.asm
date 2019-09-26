f50c934921c29b2c483a493df151213a75af6bbe
tomasabreu07 <42159630+tomasabreu07@users.noreply.github.com>
2019-09-25 21:44:15 -0300

    Faltan Leds RGB

--- Laboratorio_2.X/Laboratorio2.asm
+++ Laboratorio_2.X/Laboratorio2.asm
@@ -17,9 +17,9 @@ cblock 0x20	;Comienzo a escribir la memoria de datos en la dirección 0x20
 d0
 d1
 d2
- 
-limiteParaSetear ;limite de tiempo para setear la cuenta regresiva
+auxW
 cuentaRegresiva ;Lleva la cuenta del tiempo a ser mostrado
+segundosRestantes
 endc
 
 ;Organizacion de la memoria de programacion
@@ -36,15 +36,12 @@ main
     banksel ANSELH ;Con esto andan los botones
     clrf ANSELH
     
+    banksel segundosRestantes
+    movlw D'10'
+    movwf segundosRestantes
+    
     ;Antes de habilitar las interrupciones hay que hacer clear del reloj
-    banksel TMR1H
-    clrf TMR1H
     
-    banksel TMR1L
-    clrf TMR1L
-    
-    banksel PIR1
-    bcf PIR1,0	;Ponemos a cero el registro TMR1IF
     
     
     ;Ponemos los leds como salidas
@@ -57,39 +54,72 @@ main
     
     ;Ponemos el boton RB0 como entrada
     banksel TRISB
-    clrf TRISB
-    movlw b'10000000'
+    clrf TRISB	;Selecciono el banco de memoria de TRISC
+    movlw b'00111111'
     movwf TRISB
     
     ;generamos una variable para dar tiempo para setear
-    movlw d'255'
-    movwf limiteParaSetear
-    movwf cuentaRegresiva   
+    banksel cuentaRegresiva
     clrw
-    call activarInterrupciones
+    movwf cuentaRegresiva   
+    goto iniciar
+
+;    goto prueba
+;prueba
+;    banksel PORTB
+;    movf PORTB, w
+;    banksel PORTD
+;    movwf PORTD
+;    goto prueba
+iniciar
+    call Delay
+    banksel PORTB   
+    btfss PORTB,1
+    btfsc PORTB,1
+    goto iniciar
+    call configurar
     goto loop1
     
 loop1
     ;Hay que arreglar para que cuando termine la cuenta se activen las interrupciones
     ;pero que se pueda sumar en el medio
     call Delay
+    call encenderLeds
     banksel PORTB   
     btfss PORTB,0
     btfsc PORTB,0
     goto loop1
     goto sumarTiempo
-
- 
-activarInterrupciones
+    
+configurar
     banksel T1CON ;Configuracion del registro de timer 1 (Ver dataSheet).
-    movlw b'01110101'
+    movlw b'00110100'
     movwf T1CON
+    banksel TMR1H
+    clrf TMR1H
+    
+    banksel TMR1L
+    clrf TMR1L
+    
+    banksel PIR1
+    bcf PIR1,0	;Ponemos a cero el registro TMR1IF
     
     banksel PIE1 ;Configura el pie.
     bsf PIE1,0
     
-    bsf INTCON,6    ;Configura el peie
-    bsf INTCON,7    ;Configura el Gie.
+    bsf INTCON,6      ;Configura el peie
+    bsf INTCON,7      ;Configura el Gie.
+    
+    return
+ 
+prender
+    banksel T1CON      
+    bsf T1CON,0
+    return
+    
+apagar
+    banksel T1CON       
+    bcf T1CON,0
     return
     
 Delay ;49993 cycles
@@ -102,17 +132,28 @@ Delay ;49993 cycles
     return
 
 sumarTiempo 
+    call prender
     banksel cuentaRegresiva
     movf cuentaRegresiva,w
     ADDLW D'5'
     movwf cuentaRegresiva
     goto presionado
- 
-mostrarTiempo
+    
+encenderLeds
     banksel cuentaRegresiva
     movf cuentaRegresiva,w
     banksel PORTD
     movwf PORTD
+    return
+mostrarTiempo
+    banksel segundosRestantes
+    movlw D'10'
+    movwf segundosRestantes
+    banksel cuentaRegresiva
+    decfsz cuentaRegresiva,f
+    goto $+2
+    call apagar
+    call encenderLeds
     return
     
 presionado
@@ -124,6 +165,8 @@ presionado
     goto loop1
     
 interrupt
+    banksel auxW
+    movwf auxW
     banksel TMR1H
     clrf TMR1H
     
@@ -134,9 +177,12 @@ interrupt
     bcf PIR1,0	;Ponemos a cero el registro TMR1IF
     
     bsf INTCON,6    ;Configura el peie
-    banksel cuentaRegresiva
-    decf cuentaRegresiva
+    banksel segundosRestantes
+    decfsz segundosRestantes,f
+    goto $+2
     call mostrarTiempo
+    banksel auxW
+    movf auxW,w
     retfie
     
 END
