662dddf61b8ffc2fff4540af309318673a14c873
Bruno Garcilazo <garcilazbruno@gmail.com>
2019-10-18 20:08:43 -0300

    Commit codigo base Lab4
    Codigo provisto por los profesores.
    
    Inicializa el puerto serial
    Set de Baud Rate (asincrono)

---
+++ Laboratorio4.X/Lab4.asm
@@ -0,0 +1,46 @@
+; PIC16F887 Configuration Bit Settings
+; Assembly source line config statements
+
+#include "p16f887.inc"
+
+; CONFIG1
+; __config 0xE0F2
+ __CONFIG _CONFIG1, _FOSC_HS & _WDTE_OFF & _PWRTE_OFF & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
+; CONFIG2
+; __config 0xFEFF
+ __CONFIG _CONFIG2, _BOR4V_BOR21V & _WRT_OFF
+
+    org 0x0000
+    goto main
+ 
+eusart_init
+    banksel TXSTA
+    bsf TXSTA, TXEN  ; enable transmitter
+    bcf TXSTA, SYNC  ; asynchronous
+    banksel RCSTA
+    bsf RCSTA, CREN  ; enable receiver
+    bsf RCSTA, SPEN  ; enable EUSART, TX/CK/IO as output
+    return
+    
+; baud rate setting is is in w
+; check TABLE 12-5: BAUD RATES FOR ASYNCHRONOUS MODES
+eusart_baud_rate
+    banksel TXSTA
+    bsf TXSTA, BRGH      ; BRGH=1
+    banksel BAUDCTL
+    bcf BAUDCTL, BRG16   ; BRG16=0
+    banksel SPBRGH
+    clrf SPBRGH
+    banksel SPBRG        
+    movwf SPBRG
+    return
+ 
+main
+    movlw d'129'
+    call eusart_baud_rate
+    call eusart_init
+
+_main_loop
+    goto _main_loop
+    
+    end
